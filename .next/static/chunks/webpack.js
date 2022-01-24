80ae646581f2333fd0089df35dd1c9df5d11b053
Marcos Bastidas <marcosbastidas301@gmail.com>
2022-01-24 17:29:53 -0400

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ad276607242587b"; }
+/******/ 		__webpack_require__.h = function() { return "f800b63ab8b4df61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/molecules/Title/styled.js
+++ Components/molecules/Title/styled.js
@@ -4,5 +4,5 @@ import styled from 'styled-components'
 export const H1 = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: red !important ;
+  color: blue !important ;
 `;
